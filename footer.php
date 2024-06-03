e4e39e32c60598e6dbbf754df273944caf28fe46
Gabriellerx <166801654+Gabriellerx@users.noreply.github.com>
2024-06-03 14:07:22 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    SITE EXECUTER PAR GABRIEL LEROUX : 
+    <a class="text-dark" href="https://www.nike.com/fr/">NIKE</a>
+<tr
+<td rowspan="1" bgcolor="#FFFF00">
+<img src="LOGOVITALL.png" width="50" height="50"/>
+</tr>
+
+<input type="reset" value="VIT'ALL++" /><br /> 
+ 
+  </div>
+</footer>
